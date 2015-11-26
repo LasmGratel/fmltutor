190ce2ff87e05b5e41a1d334bf3ef1b66f86d854
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2015-11-26 20:12:55 +0800

    v2.2.2

--- src/main/java/com/github/ustc_zzzz/fmltutor/item/ItemLoader.java
+++ src/main/java/com/github/ustc_zzzz/fmltutor/item/ItemLoader.java
@@ -5,6 +5,7 @@ import com.github.ustc_zzzz.fmltutor.FMLTutor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -18,11 +19,13 @@ public class ItemLoader
 
     public static Item goldenEgg = new ItemGoldenEgg();
     public static ItemPickaxe redstonePickaxe = new ItemRedstonePickaxe();
+    public static ItemFood redstoneApple = new ItemRedstoneApple();
 
     public ItemLoader(FMLPreInitializationEvent event)
     {
         register(goldenEgg, "golden_egg");
         register(redstonePickaxe, "redstone_pickaxe");
+        register(redstoneApple, "redstone_apple");
     }
 
     @SideOnly(Side.CLIENT)
@@ -30,6 +33,7 @@ public class ItemLoader
     {
         registerRender(goldenEgg, "golden_egg");
         registerRender(redstonePickaxe, "redstone_pickaxe");
+        registerRender(redstoneApple, "redstone_apple");
     }
 
     private static void register(Item item, String name)
